6190de51ea1e140e4a0b2decd559d9b520806ef7
Tifider <Tifider@gmail.com>
2018-12-25 18:25:13 +0300

    RB-2: First commit

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,4 +1,78 @@
 package entities;
 
+import java.util.Date;
+
 public class User {
+    private int id;
+    private String name;
+    private String surname;
+    private String country;
+    private Date birthday;
+    private int phone;
+    private String email;
+    private boolean isAdmin;
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getSurname() {
+        return surname;
+    }
+    
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    
+    public String getCountry() {
+        return country;
+    }
+    
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    
+    public Date getBirthday() {
+        return birthday;
+    }
+    
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+    
+    public int getPhone() {
+        return phone;
+    }
+    
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+    
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
 }
