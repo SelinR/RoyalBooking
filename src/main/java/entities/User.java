bb470f003bbc4295933940027986de0515836bbb
Tifider <Tifider@gmail.com>
2018-12-26 15:51:42 +0300

    Added ID setter

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -15,7 +15,11 @@ public class User {
     public int getId() {
         return id;
     }
-    
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
