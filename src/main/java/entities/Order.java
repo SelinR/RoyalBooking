fb3b5ae514103b2034f373cf4c3db3f9e18fe260
Roman <roman_kotov@epam.com>
2018-12-26 19:53:41 +0300

    Added order realization

--- src/main/java/entities/Order.java
+++ src/main/java/entities/Order.java
@@ -1,4 +1,52 @@
 package entities;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Order {
+    int orderId;
+    Room bookedRoom;
+    LocalDate entryDate;
+    LocalDate leaveDate;
+    BigDecimal totalPrice;
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public Room getBookedRoom() {
+        return bookedRoom;
+    }
+
+    public void setBookedRoom(Room bookedRoom) {
+        this.bookedRoom = bookedRoom;
+    }
+
+    public LocalDate getEntryDate() {
+        return entryDate;
+    }
+
+    public void setEntryDate(LocalDate entryDate) {
+        this.entryDate = entryDate;
+    }
+
+    public LocalDate getLeaveDate() {
+        return leaveDate;
+    }
+
+    public void setLeaveDate(LocalDate leaveDate) {
+        this.leaveDate = leaveDate;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
