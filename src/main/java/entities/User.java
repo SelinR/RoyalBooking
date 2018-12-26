a3bc884f18516b22df0e31db7b81653dd7cb900e
Tifider <Tifider@gmail.com>
2018-12-26 19:12:01 +0300

    RB-2: Added full constructor

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -11,7 +11,18 @@ public class User {
     private String phone;
     private String email;
     private boolean isAdmin;
-    
+
+    public User(int id, String name, String surname, String country, LocalDate birthday, String phone, String email, boolean isAdmin) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.country = country;
+        this.birthday = birthday;
+        this.phone = phone;
+        this.email = email;
+        this.isAdmin = isAdmin;
+    }
+
     public int getId() {
         return id;
     }
