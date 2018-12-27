32761c819dc953f3dbaf18a5e56f8ce01d799c70
Mikhail Kolbasov <mikhail_kolbasov@epam.com>
2018-12-27 19:38:57 +0300

    RB-8: Created empty methods in RoomServlet

--- src/main/java/servlets/RoomServlet.java
+++ src/main/java/servlets/RoomServlet.java
@@ -1,4 +1,30 @@
 package servlets;
 
-public class RoomServlet {
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class RoomServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
 }
