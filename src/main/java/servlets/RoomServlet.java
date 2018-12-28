592c04686b4d352caf5c3a848c804988f66e1f8e
Mikhail Kolbasov <mikhail_kolbasov@epam.com>
2018-12-28 13:28:13 +0300

    RB-8: WIP: working on RoomServlet

--- src/main/java/servlets/RoomServlet.java
+++ src/main/java/servlets/RoomServlet.java
@@ -1,5 +1,8 @@
 package servlets;
 
+import entities.Room;
+import services.RoomService;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,12 +13,22 @@ public class RoomServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        req.setCharacterEncoding("UTF-8");
+        PrintWriter pw = resp.getWriter();
 
+        String query = req.getQueryString();
+        Room room = RoomService.get(query);
+        pw.println(room);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html; charset = UTF-8");
+        req.setCharacterEncoding("UTF-8");
+        PrintWriter pw = resp.getWriter();
 
+        RoomService.save(id, roomType, bedsAmount, area, dailyCost, additionalInfo);
     }
 
     @Override
