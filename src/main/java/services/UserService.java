78503a797f1a803129e7007e610a6c9c13a26d34
Tifider <Tifider@gmail.com>
2018-12-29 21:36:32 +0300

    RB-10: UserService template

--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -1,4 +1,32 @@
 package services;
 
+import dao.UserDAO;
+import entities.User;
+import enums.UserType;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class UserService {
+    private UserDAO userDAO;
+
+    public List<User> getAll() {
+
+    }
+
+    public void save() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void delete() {
+
+    }
+
+    private User create(int id, String name, String surname, String country, LocalDate birthday, String phone, String email, UserType userType) {
+       return new User(id, name, surname, country, birthday, phone, email, userType);
+    }
 }
