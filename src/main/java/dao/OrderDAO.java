281255974dde4762c1bd89d47b603341a6af67b1
Roman <roman_kotov@epam.com>
2019-01-03 21:28:53 +0300

    Added Order DAO methods. Added OrderDaoImpl implementation.

--- src/main/java/dao/OrderDAO.java
+++ src/main/java/dao/OrderDAO.java
@@ -1,4 +1,14 @@
 package dao;
 
+
+import entities.Order;
+
+import java.util.List;
+
 public interface OrderDAO {
+    List<Order> getAll();
+    Order getById(int id);
+    void save(Order order);
+    void delete(int id);
+
 }
--- src/main/java/dao/daoimpl/OrderDAOImpl.java
+++ src/main/java/dao/daoimpl/OrderDAOImpl.java
@@ -1,4 +1,53 @@
 package dao.daoimpl;
 
-public class OrderDAOImpl {
+import dao.OrderDAO;
+import entities.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDAOImpl implements OrderDAO {
+    private static OrderDAOImpl instance;
+    private List<Order> orders;
+
+    private OrderDAOImpl() {
+        orders = new ArrayList<>();
+    }
+
+    public static OrderDAOImpl getInstance() {
+        if (instance == null) {
+            instance = new OrderDAOImpl();
+        }
+        return instance;
+    }
+
+    @Override
+    public List<Order> getAll() {
+        return orders;
+    }
+
+    /**
+     * It"s temporary methods implementation, till then we are working with ArrayList
+     */
+    @Override
+    public Order getById(int id) {
+        return orders.get(id);
+    }
+
+    /**
+     * It"s temporary methods implementation, till then we are working with ArrayList
+     */
+    @Override
+    public void save(Order order) {
+        order.setId(orders.size());
+        orders.add(order);
+    }
+
+    /**
+     * It"s temporary methods implementation, till then we are working with ArrayList
+     */
+    @Override
+    public void delete(int id) {
+        orders.remove(id);
+    }
 }
