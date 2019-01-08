c26026795fac301d62ff65be596568faedf224b0
Kotov <roman_kotov@epam.com>
2019-01-08 15:03:26 +0400

    Edited init.sql. Added table creation for orders

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -22,18 +22,18 @@ INSERT INTO rooms (room_type, beds_amount, area, daily_cost, additional_info) VA
 drop table if exists orders;
 
 create table orders (
-  id serial not null,
+  id serial primary key ,
   status character varying not null,
-  booked_room_id  real not null,
+  booked_room_id  integer not null,
   entry_date date not null,
   leave_date date not null,
   total_price real not null,
-  user_id real not null
+  user_id integer not null
 );
 
 insert into orders(status, booked_room_id, entry_date, leave_date, total_price, user_id) values
-('ACCEPTED', 1, CURRENT_DATE, CURRENT_DATE + INTERVAL '1 DAY', 555, 1),
-('ACCEPTED', 3, CURRENT_DATE, CURRENT_DATE + INTERVAL '1 DAY', 555, 1),
-('ACCEPTED', 2, CURRENT_DATE, CURRENT_DATE + INTERVAL '1 DAY', 555, 1),
-('ACCEPTED', 4, CURRENT_DATE, CURRENT_DATE + INTERVAL '1 DAY', 555, 1);
+('ACCEPTED', 1, CURRENT_DATE, CURRENT_DATE + INTERVAL '1 DAY', 555.0, 1),
+('ACCEPTED', 2, CURRENT_DATE, CURRENT_DATE + INTERVAL '1 DAY', 555.0, 1),
+('ACCEPTED', 3, CURRENT_DATE, CURRENT_DATE + INTERVAL '1 DAY', 555.1, 1),
+('ACCEPTED', 4, CURRENT_DATE, CURRENT_DATE + INTERVAL '1 DAY', 555.5, 1);
 /*orders*/
