674c1616216a29833e27c00ebd6723399f9ec19d
Mikhail Kolbasov <mikhail_kolbasov@epam.com>
2019-01-11 21:33:37 +0300

    RB-46: Created WelcomeServlet, mapped it to the root "/", deleted welcome-file-list from web.xml; Deleted old RoomDAOImpl (with no jdbc).

---
+++ src/main/java/servlets/WelcomeServlet.java
@@ -0,0 +1,33 @@
+package servlets;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class WelcomeServlet extends HttpServlet {
+    private static ConfigurableApplicationContext context;
+
+    @Override
+    public void init() throws ServletException {
+        context = new ClassPathXmlApplicationContext("spring.xml");
+        System.out.println("smth");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/view/index.jsp").forward(req, resp);
+    }
+
+    public static ConfigurableApplicationContext getContext() {
+        return context;
+    }
+
+    public static void setContext(ConfigurableApplicationContext context) {
+        WelcomeServlet.context = context;
+    }
+}
