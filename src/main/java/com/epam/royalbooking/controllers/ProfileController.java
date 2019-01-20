f379c53fff9a15a5b2f5f461f5aa89b6c5ddd0e6
Tifider <Tifider@gmail.com>
2019-01-20 12:58:59 +0300

    RB-75: Created profile/details and profile/edit jsp pages. Created profile controller.

---
+++ src/main/java/com/epam/royalbooking/controllers/ProfileController.java
@@ -0,0 +1,45 @@
+package com.epam.royalbooking.controllers;
+
+import com.epam.royalbooking.entities.User;
+import com.epam.royalbooking.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ProfileController {
+    private UserService userService;
+
+    @RequestMapping("profile{id}")
+    public String getById(@PathVariable("id") int id, Model model){
+        model.addAttribute("user", userService.getById(id));
+        return "profile/details";
+    }
+
+    @RequestMapping(value = "profile/delete")
+    public String delete(int id) {
+        userService.delete(id);
+        return "home_page";
+    }
+
+    @RequestMapping("profile{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userService.getById(id));
+        return "profile/edit";
+    }
+
+    @RequestMapping(value = "profile/edit/submit", method = RequestMethod.POST)
+    public String update(@ModelAttribute("user") User user) {
+        userService.update(user);
+        return "users/one";
+    }
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/profile/details.jsp
@@ -0,0 +1,21 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Profile</title>
+</head>
+<body>
+<a href="<c:url value='..'/>">Back to previous page</a>
+<h1> Profile details </h1>
+<ul class="tg">
+    <li>Name: <c:out value="${user.name}"/></li>
+    <li>Surname: <c:out value="${user.surname}"/></li>
+    <li>Country: <c:out value="${user.country}"/></li>
+    <li>Birthday: <c:out value="${user.birthday}"/></li>
+    <li>Phone: <c:out value="${user.phone}"/></li>
+    <li>Email: <c:out value="${user.email}"/></li>
+</ul>
+<button onclick="location.href='profile/edit'">Edit</button>
+</body>
+</html>
