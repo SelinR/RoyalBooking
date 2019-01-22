9e05514d21c220e440d6388cc9769e8d1e775e50
Mikhail Kolbasov <mikhail_kolbasov@epam.com>
2019-01-22 17:23:05 +0300

    RB-79: Cleaned excess data from ApplicationConfig.

--- src/main/java/com/epam/royalbooking/config/ApplicationConfig.java
+++ src/main/java/com/epam/royalbooking/config/ApplicationConfig.java
@@ -6,39 +6,19 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-import javax.annotation.Resource;
 
 @SpringBootApplication
 @Configuration
 @EnableTransactionManagement
 @ComponentScan({"com.epam.royalbooking**"})
-@PropertySource("classpath:config.properties")
 @EntityScan({"com.epam.royalbooking.entities"})
 @EnableJpaRepositories({"com.epam.royalbooking.dao"})
 public class ApplicationConfig {
-    private static final String DATABASE_URL = "db.url";
-    private static final String DATABASE_USERNAME = "db.username";
-    private static final String DATABASE_PASSWORD = "db.password";
-
-    @Resource
-    private Environment env;
-
-    @Bean(name = "dataSource")
-    public DriverManagerDataSource dataSource() {
-        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-        driverManagerDataSource.setUrl(env.getRequiredProperty(DATABASE_URL));
-        driverManagerDataSource.setUsername(env.getRequiredProperty(DATABASE_USERNAME));
-        driverManagerDataSource.setPassword(env.getRequiredProperty(DATABASE_PASSWORD));
-        return driverManagerDataSource;
-    }
 
     @Bean
     public InternalResourceViewResolver viewResolver() {
