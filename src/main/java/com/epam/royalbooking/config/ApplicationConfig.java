e2a6ba596634a582f1d6ab83d45185fe3519ef01
Mikhail Kolbasov <mikhail_kolbasov@epam.com>
2019-01-24 18:17:37 +0300

    RB-89: Divided locale configuration and ApplicationConfig class.

--- src/main/java/com/epam/royalbooking/config/ApplicationConfig.java
+++ src/main/java/com/epam/royalbooking/config/ApplicationConfig.java
@@ -6,17 +6,10 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
-
-import java.util.Locale;
 
 
 @SpringBootApplication
@@ -25,7 +18,7 @@ import java.util.Locale;
 @ComponentScan({"com.epam.royalbooking**"})
 @EntityScan({"com.epam.royalbooking.entities"})
 @EnableJpaRepositories({"com.epam.royalbooking.dao"})
-public class ApplicationConfig implements WebMvcConfigurer {
+public class ApplicationConfig {
 
     @Bean
     public InternalResourceViewResolver viewResolver() {
@@ -34,35 +27,6 @@ public class ApplicationConfig implements WebMvcConfigurer {
         viewResolver.setPrefix("/WEB-INF/view/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
-    }
-
-    @Bean
-    public ReloadableResourceBundleMessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:locale");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
-    }
-
-    @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
-        interceptor.setParamName("lang");
-        return interceptor;
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
-    }
-
-    @Bean
-    public CookieLocaleResolver localeResolver() {
-        CookieLocaleResolver resolver = new CookieLocaleResolver();
-        resolver.setDefaultLocale(Locale.ENGLISH);
-        resolver.setCookieName("my-locale-cookie");
-        resolver.setCookieMaxAge(3600);
-        return resolver;
     }
 
     public static void main(String[] args) {
