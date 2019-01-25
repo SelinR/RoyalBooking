d4ec6ffef6efeb3b16a00d933547d0bc66966772
Artem Zubov <artem@zubov.spb.ru>
2019-01-25 23:21:28 +0300

    dev branch initial

--- src/main/java/com/epam/royalbooking/controllers/OrderController.java
+++ src/main/java/com/epam/royalbooking/controllers/OrderController.java
@@ -22,6 +22,7 @@ import java.util.List;
 
 @Controller
 public class OrderController {
+
     private OrderService orderService;
     private RoomService roomService;
     private UserService userService;
