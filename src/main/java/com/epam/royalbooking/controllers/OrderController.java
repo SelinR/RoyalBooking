ea16236fbf1b92e6aa64d27d60ceed07eee902e6
Tifider <Tifider@gmail.com>
2019-01-27 17:25:05 +0300

    RB-97: WIP

--- src/main/java/com/epam/royalbooking/controllers/OrderController.java
+++ src/main/java/com/epam/royalbooking/controllers/OrderController.java
@@ -2,7 +2,6 @@ package com.epam.royalbooking.controllers;
 
 import com.epam.royalbooking.dto.PreOrder;
 import com.epam.royalbooking.entities.Order;
-import com.epam.royalbooking.entities.Room;
 import com.epam.royalbooking.entities.User;
 import com.epam.royalbooking.services.OrderService;
 import com.epam.royalbooking.services.RoomService;
@@ -18,7 +17,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
 import java.time.LocalDate;
-import java.util.List;
 
 @Controller
 public class OrderController {
@@ -27,24 +25,13 @@ public class OrderController {
     private RoomService roomService;
     private UserService userService;
 
-    @RequestMapping(value = "/orders", method = RequestMethod.GET)
-    public String getAll(Model model) {
-        List<Order> orders = orderService.getAll();
-        List<Room> rooms = roomService.getAll();
-        model.addAttribute("rooms", rooms);
-        model.addAttribute("orders", orders);
-        model.addAttribute("minDate", LocalDate.now());
-        model.addAttribute("maxDate", LocalDate.now().plusYears(2));
-        return "orders/orders";
-    }
-
     /**
      * Needed for internationalization.
      */
-    @RequestMapping(value = "order_save/{entryDate}/{leaveDate}", method = RequestMethod.GET)
+    @RequestMapping(value = "order/save/{entryDate}/{leaveDate}", method = RequestMethod.GET)
     public ModelAndView saveGet(@ModelAttribute("order") Order order,
                                 @PathVariable("entryDate") LocalDate entryDate, @PathVariable("leaveDate") LocalDate leaveDate) {
-        ModelAndView modelAndView = new ModelAndView("orders/order_confirm");
+        ModelAndView modelAndView = new ModelAndView("orders/details");
         modelAndView.addObject("order", order);
         modelAndView.addObject("entryDate", entryDate);
         modelAndView.addObject("leaveDate", leaveDate);
@@ -58,7 +45,7 @@ public class OrderController {
      * @param order to save in DataBase
      * @param principal need to get user email and id
      */
-    @RequestMapping(value = "order_save", method = RequestMethod.POST)
+    @RequestMapping(value = "order/save", method = RequestMethod.POST)
     public String save(@ModelAttribute("order") Order order, Principal principal) {
         if (orderService.isOrderValid(order.getEntryDate(), order.getLeaveDate(), order.getBookedRoomID())) {
             String email = (principal.getName());
@@ -71,39 +58,25 @@ public class OrderController {
         }
     }
 
-    @RequestMapping(value = "/orders/delete/{id}")
-    public String delete(@PathVariable("id") int id) {
-        orderService.delete(id);
-        return "redirect:/orders";
-    }
-
-    @RequestMapping(value = "/orders/edit/{id}")
-    public String update(@PathVariable("id") int id, Model model) {
-        model.addAttribute("order", orderService.getById(id));
-        model.addAttribute("orders", orderService.getAll());
-        return "/orders";
-    }
-
     @RequestMapping(value = "/order_creation/{roomId}")
     public String getOrderCreationPage(Model model, @PathVariable("roomId") int roomId) {
         model.addAttribute("list", orderService.getAllBookedDatesByBookedRoomId(roomId));
         model.addAttribute("room", roomService.getById(roomId));
         model.addAttribute("minDate", LocalDate.now());
         model.addAttribute("maxDate", LocalDate.now().plusYears(2));
-        return "orders/order_creation";
+        return "orders/creation";
     }
 
     /**
      * Needed for internationalization.
      */
-    @RequestMapping(value = "order_confirm/{roomId}/{entryDate}/{leaveDate}", method = RequestMethod.GET)
+    @RequestMapping(value = "order/confirm/{roomId}/{entryDate}/{leaveDate}", method = RequestMethod.GET)
     public ModelAndView getOrderConfirmPageGetMethod(@ModelAttribute("order") Order order, @PathVariable("roomId") int roomId,
                                                      @PathVariable("entryDate") String entryDateVar, @PathVariable("leaveDate") String leaveDateVar,
                                                      Principal principal) {
         prepareOrderAfterLocalizationAtConfirmPage(order, entryDateVar, leaveDateVar, principal, roomId);
-        return new ModelAndView("orders/order_confirm", "order", order);
+        return new ModelAndView("orders/details", "order", order);
     }
-
 
     /**
      * Method calculates orders total price, based on dates and daily cost.
@@ -111,9 +84,9 @@ public class OrderController {
      * if user fills wrong input dates , then returns a message
      * Order isn't saved in DataBase on this stage, haven't userID
      * @param preOrder consisting of the entered dates to order
-     * @return ModelAndView with refer to order_confirm.jsp, and entity Order with calculated total price
+     * @return ModelAndView with refer to details.jsp, and entity Order with calculated total price
      */
-    @RequestMapping(value = "/order_confirm/{roomId}", method = RequestMethod.POST)
+    @RequestMapping(value = "/order/confirm/{roomId}", method = RequestMethod.POST)
     public ModelAndView getOrderConfirmPage(@ModelAttribute("preOrder") PreOrder preOrder, @PathVariable("roomId") int roomId) {
         preOrder.setEntryAndLeaveDate();
         LocalDate entryDate = preOrder.getEntryDate();
@@ -124,7 +97,7 @@ public class OrderController {
             order.setEntryDate(entryDate);
             order.setLeaveDate(leaveDate);
             order.setTotalPrice(orderService.calculateTotalPrice(order.getBookedRoomID(), order.getEntryDate(), order.getLeaveDate()));
-            return new ModelAndView("orders/order_confirm", "order", order);
+            return new ModelAndView("orders/details", "order", order);
         } else {
             return new ModelAndView("redirect:/order_creation/" + roomId + "?error");
         }
