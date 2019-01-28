0350f13006ae2f129314b70732e715c72660841f
Roman Kotov <roman_kotov@epam.com>
2019-01-28 20:55:31 +0300

    RB-102. imageSwitcher() moved to RommService.

--- src/main/java/com/epam/royalbooking/controllers/RoomController.java
+++ src/main/java/com/epam/royalbooking/controllers/RoomController.java
@@ -57,33 +57,8 @@ public class RoomController {
     public String getRoomPage(Model model, @PathVariable("roomId") int roomId) {
         model.addAttribute("room", roomService.getById(roomId));
         RoomType roomType = roomService.getById(roomId).getRoomType();
-        imageSwitcher(roomType, model);
+        roomService.imageSwitcher(roomType, model);
         return "rooms/room";
-    }
-
-    private void imageSwitcher(RoomType roomType, Model model){
-        switch (roomType ){
-            case BASIC:
-                model.addAttribute("imageUrl_1", "/media/basic1.jpg");
-                model.addAttribute("imageUrl_2", "/media/basic2.jpg");
-                model.addAttribute("imageUrl_3", "/media/basic3.jpg");
-                break;
-            case FAMILY:
-                model.addAttribute("imageUrl_1", "/media/family1.jpg");
-                model.addAttribute("imageUrl_2", "/media/family2.jpg");
-                model.addAttribute("imageUrl_3", "/media/family3.jpg");
-                break;
-            case LUXURY:
-                model.addAttribute("imageUrl_1", "/media/luxury1.jpg");
-                model.addAttribute("imageUrl_2", "/media/luxury2.jpg");
-                model.addAttribute("imageUrl_3", "/media/luxury3.jpg");
-                break;
-            case PENTHOUSE:
-                model.addAttribute("imageUrl_1", "/media/pent1.jpg");
-                model.addAttribute("imageUrl_2", "/media/pent2.jpg");
-                model.addAttribute("imageUrl_3", "/media/pent3.jpg");
-                break;
-        }
     }
 
     @Autowired
